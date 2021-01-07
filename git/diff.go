b9515532e9192289574fe26f4acc179d15b17d9f
Joseph Naberhaus <naberhausj@gmail.com>
2021-01-07 11:21:15 -0600

    fix(git): fix program starting up slowly when there is a large diff

--- git/diff.go
+++ git/diff.go
@@ -3,7 +3,7 @@ package git
 import "os/exec"
 
 func AreStagedFiles() bool {
-	cmd := exec.Command("git", "diff", "--cached", "--exit-code")
+	cmd := exec.Command("git", "diff", "--quiet", "--cached", "--exit-code")
 
 	_, err := cmd.Output()
 	return err != nil
